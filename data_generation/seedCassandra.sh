ea3ea86b099fed035c9c66b889526173e8135097
Rudy <rudydimacali@gmail.com>
2019-01-11 16:42:18 -0800

    Complete Postgres seeding script.

--- data_generation/seedCassandra.sh
+++ data_generation/seedCassandra.sh
@@ -1,8 +1,8 @@
 counter=1
 while [ $counter -le 11 ]
   do
-    sed -i '' s/[0-9][0-9]*/$counter/g ./seed.cql
+    sed -i '' s/[0-9][0-9]*/$counter/g ./data_generation/seed.cql
     # cat ./seed.cql
-    cqlsh -f ./seed.cql
+    cqlsh -f ./data_generation/seed.cql
   ((counter++))
 done
