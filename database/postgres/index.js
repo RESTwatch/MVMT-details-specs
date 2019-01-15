d8cfb5fee57170700147ddb5099005ce928f5606
Rudy <rudydimacali@gmail.com>
2019-01-14 19:55:51 -0800

    Add newrelic.js.

--- database/postgres/index.js
+++ database/postgres/index.js
@@ -5,8 +5,8 @@ const config = require('./config.js');
 const client = new Client(config.postgresConfig);
 client.connect();
 
-client.query('SELECT * FROM detail', (err, res) => {
-  if (err) { throw err; }
-  console.log(res);
-  client.end()
-});
+// client.query('SELECT * FROM detail', (err, res) => {
+//   if (err) { throw err; }
+//   console.log(res);
+//   client.end()
+// });
