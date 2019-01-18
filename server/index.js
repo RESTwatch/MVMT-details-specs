4b035316a32dd87806059a6d1a3d9e41badd96c6
Rudy <rudydimacali@gmail.com>
2019-01-18 11:55:49 -0800

    Complete connection to ec2 postgres/redis.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 require('newrelic');
 const express = require('express');
 const morgan = require('morgan');
-// const { getSpecsForId, addWatch, updateWatch, deleteWatch } = require('../database/index.js');
 const { getSpecsForId } = require('../database/postgres/index.js');
 
 const app = express();
@@ -10,11 +9,11 @@ app.use(morgan('tiny'));
 app.use('/watches/:wid', express.static('client/dist'));
 
 
-app.post('/api/watches/:wid/:name', (req, res) => {
-  addWatch(req.params.wid, req.params.name, () => {
-    res.send('Success creating watch!');
-  });
-});
+// app.post('/api/watches/:wid/:name', (req, res) => {
+//   addWatch(req.params.wid, req.params.name, () => {
+//     res.send('Success creating watch!');
+//   });
+// });
 
 app.get('/api/watches/:wid/details', (req, res) => {
   const id = req.params.wid;
@@ -27,17 +26,17 @@ app.get('/api/watches/:wid/details', (req, res) => {
   });
 });
 
-app.put('/api/watches/:wid/:detailName=:detail', (req, res) => {
-  updateWatch(req.params.wid, req.params.detailName, req.params.detail, () => {
-    res.send(`Successfully updated watch ${req.params.detailName}.`);
-  });
-});
+// app.put('/api/watches/:wid/:detailName=:detail', (req, res) => {
+//   updateWatch(req.params.wid, req.params.detailName, req.params.detail, () => {
+//     res.send(`Successfully updated watch ${req.params.detailName}.`);
+//   });
+// });
 
-app.delete('/api/watches/:wid/', (req, res) => {
-  deleteWatch(req.params.wid, () => {
-    res.send(`Sucessfully deleted watch #${req.params.wid}.`);
-  });
-});
+// app.delete('/api/watches/:wid/', (req, res) => {
+//   deleteWatch(req.params.wid, () => {
+//     res.send(`Sucessfully deleted watch #${req.params.wid}.`);
+//   });
+// });
 
 app.listen(3003, () => {
   console.log('Listening on port 3003...');
